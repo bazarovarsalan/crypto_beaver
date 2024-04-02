96f11b27bf8ac25fbc0590dd2544ec877874e00e
Arsalan Bazarov <bazarovarsalan@gmail.com>
2024-04-03 00:11:16 +0800

    url changed

--- src/components/main/Market.tsx
+++ src/components/main/Market.tsx
@@ -9,7 +9,7 @@ import ErrorComponent from "../ErrorComponent";
 
 function Market() {
   const [page, setPage] = useState<number>(1);
-  const url = `api/markets?vs_currency=usd&order=market_cap_desc&per_page=10&pag=${page}&sparkline=false`;
+  const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&pag=${page}&sparkline=false`;
 
   const { data, loading, error }: fetchCryptocurrencies = useFetch(url);
   const navigate = useNavigate();
